ea2a5d4061271dc2f82d0a77cbc181c3882a20a9
neiger <neiger.serrano.wks@gmail.com>
2025-01-24 14:22:13 -0600

    git local repo project

---
+++ XCUITestAutomation/XCUITestAutomationApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCUITestAutomationApp.swift
+//  XCUITestAutomation
+//
+//  Created by Neiger Serrano on 24/1/25.
+//
+
+import SwiftUI
+
+@main
+struct XCUITestAutomationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XCUITestAutomationTests/XCUITestAutomationTests.swift
@@ -0,0 +1,16 @@
+//
+//  XCUITestAutomationTests.swift
+//  XCUITestAutomationTests
+//
+//  Created by Neiger Serrano on 24/1/25.
+//
+
+import Testing
+
+struct XCUITestAutomationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
